1b6ed0f8025c40265e2ab80d5b51bbdb4f071fb7
Brian Kenneth Blaylock <u0553130@ad.utah.edu>
2018-02-13 08:49:16 -0700

    updates

--- js/pando_status.js
+++ js/pando_status.js
@@ -3,6 +3,21 @@ document.write('\
     <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>\
     <img src="./images/sick.png" align=left style="padding-right:0px; width:75px">\
     <div style="padding-left:80px"><big>\
+        <p>The Pando archive system has been rebuilt with data starting December 28, 2017.\
+        We are working with a private company to potentially recover HRRR runs from 2016-2017, so check back later.\
+        <br><a class="alert-link" href="http://home.chpc.utah.edu/~u0553130/Brian_Blaylock/Pando_archive/Pando_Failure.html">\
+            More Details on the Pando archive failure</a>\
+        </big></div>\
+</div>\
+');
+
+
+/* Pando Failure
+document.write('\
+<div class="alert alert-danger">\
+    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>\
+    <img src="./images/sick.png" align=left style="padding-right:0px; width:75px">\
+    <div style="padding-left:80px"><big>\
         <p>The Pando archive system failed and we are in the process of rebuilding it.\
         <br>Operational HRRR files are now available starting December 28, 2017.\
         <br>You can still register as a user and we will email you when Pando back up.\
@@ -11,3 +26,4 @@ document.write('\
         </big></div>\
 </div>\
 ');
+*/
