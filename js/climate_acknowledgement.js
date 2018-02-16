8cb9583fb69b6566625fc0bde5c14ceec0b08942
Brian Kenneth Blaylock <u0553130@ad.utah.edu>
2018-02-16 09:21:17 -0700

    updates

---
+++ js/climate_acknowledgement.js
@@ -0,0 +1,9 @@
+/*Pando Rebuilt*/
+document.write('\
+<div class="alert alert-success">\
+    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>\
+    <img src="./images/climate-corporation2.jpg" align=left style="padding-right:0px;">\
+        <p>HRRR data prior to 2018 is available because of a generous contribution \
+        from <a href="https://climate.com/" class="alert-link">The Climate Corporation</a>.\
+</div>\
+');
