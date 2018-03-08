ff4dc41c00c82209c677617573a453e7d3ddac53
Brian Kenneth Blaylock <u0553130@ad.utah.edu>
2018-03-08 09:45:15 -0700

    updates

--- js/pando_status.js
+++ js/pando_status.js
@@ -1,15 +1,5 @@
 /*Pando Rebuilt*/
-document.write('\
-<div class="alert alert-info">\
-    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>\
-    <img src="./images/happy.png" align=left style="padding-right:0px; width:75px">\
-    <div style="padding-left:80px"><big>\
-        <p>The Pando archive system was rebuilt with data starting 28 December 2017.\
-        We are working with a private company to recover HRRR runs beginning 15 July 2016, so check back soon.\
-        <br><a class="alert-link" href="http://home.chpc.utah.edu/~u0553130/Brian_Blaylock/Pando_archive/Pando_Failure.html">\
-            More Details on the Pando archive failure</a>\
-        </big></div>\
-</div>\
+document.write('
 ');
 
 
@@ -25,6 +15,22 @@ document.write('\
         <br>You can still register as a user and we will email you when Pando back up.\
         <br><a class="alert-link" href="http://home.chpc.utah.edu/~u0553130/Brian_Blaylock/Pando_archive/Pando_Failure.html">\
             More Details HERE</a>\
+        </big></div>\
+</div>\
+');
+*/
+
+/* Pando Rebuilt*/
+/*
+document.write('\
+<div class="alert alert-info">\
+    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>\
+    <img src="./images/happy.png" align=left style="padding-right:0px; width:75px">\
+    <div style="padding-left:80px"><big>\
+        <p>The Pando archive system was rebuilt with data starting 28 December 2017.\
+        We are working with a private company to recover HRRR runs beginning 15 July 2016, so check back soon.\
+        <br><a class="alert-link" href="http://home.chpc.utah.edu/~u0553130/Brian_Blaylock/Pando_archive/Pando_Failure.html">\
+            More Details on the Pando archive failure</a>\
         </big></div>\
 </div>\
 ');
