6e902b1f47627e4d69e79f5e8ce07b413b3ba275
Brian Blaylock <u0553130@kingspeak33.wasatch.peaks>
2020-10-13 10:51:28 -0600

    updates

--- js/site/siteclose.js
+++ js/site/siteclose.js
@@ -2,12 +2,12 @@ document.write('\
 <br>\
 <div id="footer">\
 <p style="text-align:center">\
-This page is maintained by Brian Blaylock &copy 2019, \
+This page is maintained by University of Utah &copy 2020, \
  <a href="http://www.atmos.utah.edu/" style="color:#990000; text-decoration:none">Department of Atmospheric Science, University of Utah</a>. \
  <a href="https://twitter.com/blaylockbk" style="color:#990000; text-decoration:none"><big><i class="fab fa-twitter fa-fw"></i></big> </a>\
  <a href="https://github.com/blaylockbk" style="color:#990000; text-decoration:none"><big><i class="fab fa-github fa-fw"></i></big> </a>\
  <a style="color:#990000; text-decoration:none; font-weight:700" href="mailto:brian.blaylock@utah.edu">\
-<big><i class="fas fa-envelope fa-fw"></i></big> brian.blaylock@utah.edu\
+<big><i class="fas fa-envelope fa-fw"></i></big> atmos-mesowest-request@lists.utah.edu\
 </a>\
 </p>\
 </div>\
